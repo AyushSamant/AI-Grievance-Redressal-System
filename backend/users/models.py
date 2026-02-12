7fb2c8aded1cf024356431ba2ecef97cbf4db427
Ayush Singh Samant <ayushsamant01@gmail.com>
2026-02-12 22:58:24 +0530

    Configured JWT auth, updated user model, updated settings and gitignore

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,15 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-# Create your models here.
 
-class User(AbstractUser):
+# Create your models here.
+# Creating a Custom User Model
+# instead of deafult user auth.User we are creating are own users.User as we need roles + future scalability
+# Abstract Users - django default built-in-user model
+
+class User(AbstractUser): # taking django's default User and adding more fields
     ROLE_CHOICES = (
         ("ADMIN", "Admin"),
         ("CITIZEN", "Citizen"),
         ("OFFICER", "Department Officer"),
     )
 
-    role = models.CharField(
+    #every user has a role
+    role = models.CharField( 
         max_length=20,
         choices=ROLE_CHOICES,
         default="CITIZEN"
@@ -19,3 +24,6 @@ class User(AbstractUser):
 
     def __str__(self):
         return f"{self.username} - {self.role}"
+    
+"""Note: Since my AI-powered grievance system requires role-based access control and future authentication scalability, 
+    I extended Django’s AbstractUser to include role management and additional metadata fields"""
